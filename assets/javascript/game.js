761eeeffb3be70f5595d72605b99792a43763c4b
Karina Solorio <macuser@Karinas-MacBook-Pro.local>
2018-04-07 21:41:27 -0400

    minor changes

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -12,50 +12,60 @@ $(document).ready(function() {
     
     // each crystal should have random number btw 1-12
     var gemOne = (Math.floor(Math.random() * 12) + 1);
-        console.log(gemOne);
+        console.log('1 = ' + gemOne);
     var gemTwo = (Math.floor(Math.random() * 12) + 1);
-        console.log(gemTwo);
+        console.log('2 = ' + gemTwo);
     var gemThree = (Math.floor(Math.random() * 12) + 1);
-        console.log(gemThree);
+        console.log('3 = ' + gemThree);
     var gemFour = (Math.floor(Math.random() * 12) + 1);
-        console.log(gemFour);
+        console.log('4 = ' + gemFour);
 
 
     //console log what the random number is
-    console.log(randomNumber);
+    console.log('computer# = ' + randomNumber);
 
     //displaying the random number
     $('.random-number').text(randomNumber);
 
 // player clicks on crystal
-    $("#crystal-1", "#crystal-2", "#crystal-3", "#crystal-4").on("click", function (count){
-        gemOne ;
-        gemTwo;
-        gemThree;
-        gemFour;
-        console.log(gemOne, gemTwo, gemThree, gemFour)
-    })
+    $('#crystal-1').on('click', function(){
+        result = gemCount + gemOne;
+        $('.crystal-count').text(gemCount);
+        
+                 
+    
+    
+         
+             
 
-    count;
+    })
+    
 
 // this will update the players total score
 
 // player wins if their total matches the random number
-
+function winner() {
+    wins++;
+    $('#wins').text(wins);
+    reset();
+}
 // player loses if score goes above the random number
-
+function loser () {
+    losses ++;
+    $('#losses').text(losses);
+    reset();
+}
 // GAME RESET
 function reset() {
     randomNumber = (Math.floor(Math.random() *101) + 19);
-    console.log(randomNumber);
+    console.log('new # = ' + randomNumber);
     $('.random-number').text(randomNumber);
     var gemOne = (Math.floor(Math.random() * 12) + 1);
     var gemTwo = (Math.floor(Math.random() * 12) + 1);
     var gemThree = (Math.floor(Math.random() * 12) + 1);
     var gemFour = (Math.floor(Math.random() * 12) + 1);
-    $('.random-number').text(randomNumber);
     totalScore = 0;
-
+    gemCount = 0;
 }
 
 });
